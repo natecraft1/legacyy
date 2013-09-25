bb41702ef29ac8e0db1dc37d68adb5649f5329fc
nate dawg <nglass1988@gmail.com>
2013-09-24 22:53:21 -0700

    deleted pictures from git and introducted parallax

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,16 @@
 class PagesController < ApplicationController
 	def home
 		@users = User.all
+		@first_user = User.first
+		sort = @first_user.years.sort_by { |year| 
+			year.year.to_i
+		}
+		@image_array = []
+		sort.map {|x|  
+			unless x.avatar_file_name.nil? 
+				@image_array << x
+			end 
+		}
 		@usernames = @users.collect { |item| item.name }
 	end
 end
